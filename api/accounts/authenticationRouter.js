e4c9fd6c46f2b8b8e83b5de24bdb5d572759260c
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-07-01 14:54:53 -0700

    signup route

--- api/accounts/authenticationRouter.js
+++ api/accounts/authenticationRouter.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
+router.post("/signup", async (req, res) => {
+  try {
+    const result = await registerUser(req.User, userType);
+    res.status(201).json({ message: result });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error in Signup, User creation Failed.",
+      error_message: error.message,
+    });
+  }
+});
+
 module.exports = router;
