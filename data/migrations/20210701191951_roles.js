1fe06834610f815dc6b85209a241ed59b85e0b9b
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-07-01 13:04:53 -0700

    creating tables in knex-migration file

--- data/migrations/20210701191951_roles.js
+++ data/migrations/20210701191951_roles.js
@@ -1,8 +1,42 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("roles", (roles) => {
+      roles.increments("role_id");
+      roles.string("name", 255).notNullable().unique();
+    })
+    .createTable("departments", (department) => {
+      department.increments("department_id");
+      department.string("name").notNullable();
+    })
+    .createTable("users", (users) => {
+      users.increments("id");
+      users.string("first_name", 255).notNullable();
+      users.string("last_name", 255).notNullable();
+      users.string("email", 255).notNullable().unique();
+      users.string("password", 255).notNullable();
+      users
+        .integer("role")
+        .unsigned()
+        .notNullable()
+        .references("role_id")
+        .inTable("roles")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      users
+        .integer("department")
+        .unsigned()
+        .notNullable()
+        .references("department_id")
+        .inTable("departments")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      users.integer("pinpoint");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("roles")
+    .dropTableIfExists("users")
+    .dropTableIfExists("departments");
 };
