43c5a9fd25646eb4e4fbb8fb623e29442b6969cf
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-07-02 13:54:44 -0700

    removed buildUser, will handle department and role on client side before form submisson

--- api/auth/registerUser.js
+++ api/auth/registerUser.js
@@ -1,16 +1,13 @@
 const bcrypt = require("bcrypt");
-
 const UserHelper = require("../models/userModel.js");
-const buildUser = require("../auth/registerUserType.js");
 
 const { genSalt, hash } = bcrypt;
 async function registerUser(user, type) {
   try {
     const salt = await genSalt(11);
     const hashedPassword = await hash(user.password, salt);
-    const newUser = await buildUser(user, type, hashedPassword);
+    const newUser = { ...user, password: hashedPassword };
     const success = await UserHelper.createUser(newUser);
-
     return success;
   } catch (error) {
     throw new Error({
